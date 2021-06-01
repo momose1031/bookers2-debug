5c0d4d0181699e5940b45920500669a67828def9
momose1031 <macbookair2020momose@icloud.com>
2021-06-01 09:44:01 +0000

    [finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,21 @@
 class BooksController < ApplicationController
 
-  def show
-    @book = Book.find(params[:id])
-  end
-
   def index
     @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book_new = Book.new
+    @book = Book.find(params[:id])
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
     else
       @books = Book.all
       render 'index'
@@ -21,6 +24,10 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+    else
+      redirect_to books_path
+    end
   end
 
 
@@ -28,22 +35,23 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book), notice: "You have updated book successfully."
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book)
     else
       render "edit"
     end
   end
 
-  def delete
-    @book = Book.find(params[:id])
-    @book.destoy
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 
 end
